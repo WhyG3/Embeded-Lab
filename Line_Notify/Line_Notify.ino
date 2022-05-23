173147cb3c6f25948670e1e4a43c05aa5338e37b
WhyG3 <why555553@gmail.com>
2022-05-23 17:11:04 +0700

    Line notify

--- Line_Notify/Line_Notify.ino
+++ Line_Notify/Line_Notify.ino
@@ -4,39 +4,30 @@ send temperature value to line notify
 // setup
 #include <WiFi.h>
 #include <Wire.h>
-#define LM73_ADDR 0x4D // Kidbright's temerature sensor address
-#define DELAY 5000     // 5 s
+
 // setup line API
 #include <TridentTD_LineNotify.h>
-#define LINE_TOKEN "WoU89RimCydkbdRdRQingDYEHIKrJ6RcYvdbwbA0HJb"
+#define LINE_TOKEN "Ther4dP40dpBluO4LLATCrAwEM21Vh1k1nZw29qH0su"
+
 // setup WIfi
 char ssid[] = "casa21"; // Wifi's name
 char pass[] = "";       // Wifi's password
+// set up global variable
+#define LM73_ADDR 0x4D // Kidbright's temerature sensor address
+#define DELAY 10000     // 10 s
+float data;
+float readTemperature();
 
 void setup()
 {
   // put your setup code here, to run once:
   Serial.begin(115200);
+  Wire1.begin(4, 5);
   Serial.println(LINE.getVersion());
   connectWifi();  // Connect to Wi-Fi network.
   // connect Line API service
   LINE.setToken(LINE_TOKEN);
-  LINE.notify("Hellow");
-  // sent number to line 
-  int va = 25;
-  float pi = 3.14159265359;
-  float temp = 39.12345;
-  LINE.notify(va);    // interger number
-  LINE.notify(pi, 4); // 4 digit float number 
-
-  // sent text to line
-  LINE.notify("Temp = " + String(temp, 3) + " C");
-
-  // sent sticket to line
-  LINE.notifySticker("อุณหภูมิสูง", 4, 274); // sent Line Sticker by PackageID 1 , StickerID 2
-
-  // sent image by url
-  LINE.notifyPicture("https://cq.lnwfile.com/_/cq/_raw/0w/1o/gu.jpg");
+  LINE.notify("Hello");
 }
 
 void loop()
@@ -46,6 +37,12 @@ void loop()
   {
     connectWifi();
   }
+  
+  data = readTemperature();
+  Serial.println(data);
+  LINE.notify("Temperature "+String(data)+" C");
+  delay(DELAY);
+  
 }
 
 // Function connect Wifi
@@ -65,3 +62,23 @@ void connectWifi()
     Serial.println(WiFi.localIP());
   }
 }
+// read temp sensor function
+float readTemperature() {
+  Wire1.beginTransmission(LM73_ADDR);
+  Wire1.write(0x00);
+  Wire1.endTransmission();
+ 
+  uint8_t count = Wire1.requestFrom(LM73_ADDR, 2);
+  float temp = 0.0;
+  if (count == 2) {
+    byte buff[2];
+    buff[0] = Wire1.read();
+    buff[1] = Wire1.read();
+    temp += (int)(buff[0]<<1);
+    if (buff[1]&0b10000000) temp += 1.0;
+    if (buff[1]&0b01000000) temp += 0.5;
+    if (buff[1]&0b00100000) temp += 0.25;
+    if (buff[0]&0b10000000) temp *= -1.0;
+  }
+  return temp;
+} 
